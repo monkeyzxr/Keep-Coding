ca37b8fcde5c066ed649d1f686a476d551805346
monkeyzxr <monkeyzxr@gmail.com>
2017-06-08 23:45:35 -0500

    Add files via upload

--- LC_125_ValidPalindrome.java
+++ LC_125_ValidPalindrome.java
@@ -18,6 +18,7 @@
 //http://www.programcreek.com/2013/01/leetcode-valid-palindrome-java/
     //https://www.liuchuo.net/archives/1136
     //思路简单：但是我写的一直报错。没找出原因。
+    /*
 public class LC_125_ValidPalindrome {
     public static boolean isPalindrome(String s) {
         if (s == null || s.length() == 0)
@@ -62,6 +63,84 @@ public class LC_125_ValidPalindrome {
 
         System.out.println(isPalindrome(s1));
         //System.out.println(isPalindrome(s2));
+
+    }
+}
+
+*/
+
+
+    //我重新写：
+    //成功：
+    /*
+public class LC_125_ValidPalindrome {
+    public static boolean equal(char x, char y) {
+        if (x == y) return true;
+        if (((x <= '9') && (x >= '0')) || ((y <= '9') && (y >= '0'))) return false;
+        if (Math.abs(x - y) == Math.abs('A' - 'a')) return true;
+        return false;
+    }
+
+    public static boolean valid(char x) {
+        if (((x <= 'z') && (x >= 'a')) || ((x <= 'Z') && (x >= 'A')) || ((x <= '9') && (x >= '0'))) return true;
+        return false;
+    }
+
+    public static boolean isPalindrome(String s) {
+
+        if (s.length() <= 1) return true;
+
+        int lo = 0;
+        int hi = s.length() - 1;
+        while ((lo < s.length()) && (hi >= 0)){
+            while ((lo < s.length()) && !valid(s.charAt(lo))) lo++;
+            while ((hi >= 0)         && !valid(s.charAt(hi))) hi--;
+            if ((lo == s.length()) || (hi == -1)) break;
+            if (!equal(s.charAt(lo++), s.charAt(hi--))) return false;
+        }
+        return true;
+
+
+
+    }
+
+    public static void main(String[] args) {
+        String s1 = "A man, a plan, a canal: Panama";
+        String s2 = "race a car";
+
+        System.out.println(isPalindrome(s1));
+        System.out.println(isPalindrome(s2));
+
+    }
+}
+*/
+
+    //网上做法：用正则表达式
+public class LC_125_ValidPalindrome {
+
+    public static boolean isPalindrome(String s) {
+        //[...]	Matches any single character in brackets.
+        //[^...]	Matches any single character not in brackets.
+        String regex = "([^A-Za-z0-9])";
+        String replacement = "";
+        s = s.replaceAll(regex, replacement);  //把s中的所有非A-Za-z0-9的，都替换成""
+        for(int i =0;i<s.length()/2;i++){
+            if(Character.toLowerCase(s.charAt(i))==Character.toLowerCase(s.charAt(s.length()-1-i)))
+                continue;
+            else
+                return false;
+        }
+        return true;
+
+
+    }
+
+    public static void main(String[] args) {
+        String s1 = "A man, a plan, a canal: Panama";
+        String s2 = "race a car";
+
+        System.out.println(isPalindrome(s1));
+        System.out.println(isPalindrome(s2));
 
     }
 }

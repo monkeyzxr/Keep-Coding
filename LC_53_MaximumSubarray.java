26f076b8a436b42a735134c53a5068cdeb3d2fad
monkeyzxr <monkeyzxr@gmail.com>
2017-06-13 22:53:06 -0500

    Add files via upload

--- LC_53_MaximumSubarray.java
+++ LC_53_MaximumSubarray.java
@@ -134,3 +134,39 @@ public class LC_53_MaximumSubarray {
     }
 }
 
+/*
+
+http://www.programcreek.com/2013/02/leetcode-maximum-subarray-java/
+
+The changing condition for dynamic programming is
+"We should ignore the sum of the previous n-1 elements if nth element is greater than the sum."
+
+public class Solution {
+	public int maxSubArray(int[] A) {
+		int max = A[0];
+		int[] sum = new int[A.length];
+		sum[0] = A[0];
+
+		for (int i = 1; i < A.length; i++) {
+			sum[i] = Math.max(A[i], sum[i - 1] + A[i]);
+			max = Math.max(max, sum[i]);
+		}
+
+		return max;
+	}
+}
+
+
+  public int maxSubArray(int[] A) {
+       int newsum=A[0];
+       int max=A[0];
+       for(int i=1;i<A.length;i++){
+           newsum=Math.max(newsum+A[i],A[i]);
+           max= Math.max(max, newsum);
+       }
+       return max;
+    }
+
+
+ */
+

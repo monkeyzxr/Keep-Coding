d2fd8b31b85e66f837fad821af987080d225193f
monkeyzxr <monkeyzxr@gmail.com>
2017-07-11 23:05:07 -0500

    Add files via upload

--- LC_96_UniqueBinarySearchTrees.java
+++ LC_96_UniqueBinarySearchTrees.java
@@ -58,12 +58,52 @@ f(0) = 1
 f(n) = f(0)*f(n-1) + f(1)*f(n-2) + ... + f(n-2)*f(1) + f(n-1)*f(0)
 
  */
+
+/*
+解法 //  https://www.tianmaying.com/tutorial/LC96
+
+本题是一道比较容易的动态规划，对于一颗二叉树，简单来说可以分为：
+
+     根节点
+    /     \
+ 左子树   右子树
+假如整个树有 n 个节点，根节点为 1 个节点，两个子树平分剩下的 n-1 个节点。
+
+假设我们已经知道节点数量为 x 的二叉树有dp[x]种不同的形态。
+
+则一颗二叉树左节点节点数量为 k 时，其形态数为dp[k] * dp[n - 1 - k]。
+
+而对于一颗 n 个节点的二叉树，其两个子树分配节点的方案有 n-1 种：
+
+(0, n-1), (1, n-2), ..., (n-1, 0)
+因此我们可以得到对于 n 个节点的二叉树，其形态有：
+
+Sigma(dp[i] * dp[n-1-i]) | i = 0 .. n-1
+并且可以发现，dp数组有递推关系，我们可以使用递推或是记忆化搜索来实现。
+
+边界条件为dp[0] = 1。
+ */
 public class LC_96_UniqueBinarySearchTrees {
     public static int numTrees(int n) {
+        //定义数组
+        int [] dp = new int [n+1];
+
+        //初始化
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++){
+            //用j来循环除了root之外的子树node,例如，想象j是左子树的node个数
+            for (int j = 0; j <= i-1; j++){  //i-1，因为root已经确定了，只看子树
+                dp[i] += dp[j] * dp[i-1-j];
+            }
+        }
+        return dp[n];
 
     }
 
     public static void main(String[] args) {
+        System.out.println(numTrees(3));
 
     }
 }

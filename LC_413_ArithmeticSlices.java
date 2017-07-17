703e887b8905b66d2d50816d33821b864a3d0f22
monkeyzxr <monkeyzxr@gmail.com>
2017-07-16 22:33:07 -0500

    Add files via upload

--- LC_413_ArithmeticSlices.java
+++ LC_413_ArithmeticSlices.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Created by monkeyzxr on 2017/7/15.
  *
@@ -37,13 +39,101 @@
        // 那么我们可以找出递推式，长度为n的等差数列中含有长度至少为3的算数切片的个数为(n-1)(n-2)/2
     //那么题目就变成了找原数组中等差数列的长度，然后带入公式去算个数即可
 
+    //http://blog.csdn.net/mebiuw/article/details/52876206
+    //我认为，就是求数组中 等差数列的大于3的长度
+
+    /*
 public class LC_413_ArithmeticSlices {
     public static int numberOfArithmeticSlices(int[] A) {
-        int len = 2
+        int res = 0;
+        int len = 2; //初始化为2， 认为是等差数列两端的数。len大约等于3才能 套公式
+
+        for (int i = 2; i < A.length; i++){  //公式里有A【i-2】，所以，i要从2开始循环
+            if (A[i]-A[i-1] == A[i-1]-A[i-2])  //在找一段连续的等差数列的长度
+                len++;
+            else {
+                if (len > 2)
+                    res += (len-1)*(len-2)*0.5;
+
+                len = 2; // 等差数列的长度恢复初始化2
+            }
+        }
+
+        if (len > 2)
+            res += (len-1)*(len-2)*0.5;
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] A = {1,2,3,4,5};
+        System.out.println(numberOfArithmeticSlices(A));
+
+    }
+}
+*/
+
+    /*
+    用DP来做，定义一个一维dp数组，其中dp[i]表示，到i位置为止的算数切片的个数，
+    那么我们从第三个数字开始遍历，如果当前数字和之前两个数字构成算数切片，那么我们更新dp[i]为dp[i-1]+1，然后res累加上dp[i]的值即可
+     */
+
+    /*
+public class LC_413_ArithmeticSlices {
+    public static int numberOfArithmeticSlices(int[] A) {
+        int res = 0;
+        int[] dp = new int[A.length];
+        Arrays.fill(dp, 0);
+
+        for (int i = 2; i < A.length; i++){
+            if (A[i]-A[i-1] == A[i-1]-A[i-2])
+                dp[i] = dp[i-1] + 1;
+
+            res += dp[i];
+        }
+        return res;
 
     }
 
     public static void main(String[] args) {
+        int[] A = {1,2,3,4,5};
+        System.out.println(numberOfArithmeticSlices(A));
 
     }
+}
+    */
+
+
+    //重新写DP
+    //https://discuss.leetcode.com/topic/62992/3ms-c-standard-dp-solution-with-very-detailed-explanation
+    //这个更好
+public class LC_413_ArithmeticSlices {
+    public static int numberOfArithmeticSlices(int[] A) {
+        if (A.length < 3)
+            return 0;
+
+        int[] dp = new int[A.length];
+        Arrays.fill(dp, 0);// dp[i] means the number of arithmetic slices ending with A[i]
+
+        //初始化
+        if (A[2] - A[1] == A[1] - A[0])// if the first three numbers are arithmetic or not
+            dp[2] = 1;
+
+        int result = dp[2];
+
+        for (int i = 3; i < A.length; i++) {
+            if (A[i] - A[i - 1] == A[i - 1] - A[i - 2])
+                dp[i] = dp[i - 1] + 1;
+
+            result += dp[i];
+        }
+        return result;
+
+    }
+
+    public static void main(String[] args) {
+        int[] A = {1, 2, 3, 4, 5};
+        System.out.println(numberOfArithmeticSlices(A));
+
+    }
+
 }

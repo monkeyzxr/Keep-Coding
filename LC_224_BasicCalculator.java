7c674481e2d03c9403cecbb1192f65e6369457c8
monkeyzxr <monkeyzxr@gmail.com>
2017-07-18 23:01:18 -0500

    Add files via upload

---
+++ LC_224_BasicCalculator.java
@@ -0,0 +1,27 @@
+/**
+ * Created by monkeyzxr on 2017/7/18.
+ *
+ Implement a basic calculator to evaluate a simple expression string.
+
+ The expression string may contain open ( and closing parentheses ), the plus + or minus sign -, non-negative integers and empty spaces .
+
+ You may assume that the given expression is always valid.
+
+ Some examples:
+ "1 + 1" = 2
+ " 2-1 + 2 " = 3
+ "(1+(4+5+2)-3)+(6+8)" = 23
+ Note: Do not use the eval built-in library function.
+ *
+ */
+
+
+public class LC_224_BasicCalculator {
+    public static int calculate(String s) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

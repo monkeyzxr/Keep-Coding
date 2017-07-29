734183c3a0c0a77fdac0a073759b0d6121599360
monkeyzxr <monkeyzxr@gmail.com>
2017-07-28 23:54:01 -0500

    Add files via upload

---
+++ LC_515_FindLargestValueEachTreeRow.java
@@ -0,0 +1,36 @@
+import java.util.List;
+
+/**
+ * Created by monkeyzxr on 2017/7/28.
+ *
+ You need to find the largest value in each row of a binary tree.
+
+ Example:
+ Input:
+
+      1
+     / \
+    3   2
+   / \   \
+  5   3   9
+
+ Output: [1, 3, 9]
+ *
+ */
+
+public class LC_515_FindLargestValueEachTreeRow {
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+  }
+
+    public List<Integer> largestValues(TreeNode root) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

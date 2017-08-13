1f779a946eccec77d429c9a3e6557fed149ebea3
monkeyzxr <monkeyzxr@gmail.com>
2017-08-12 22:51:11 -0500

    Add files via upload

--- LC_143_ReorderList.java
+++ LC_143_ReorderList.java
@@ -35,15 +35,79 @@ http://www.jianshu.com/p/679d4a435b6a
 
 //http://www.jiuzhang.com/solutions/reorder-list/
 public class LC_143_ReorderList {
-    public class ListNode {
+    public static class ListNode {
       int val;
       ListNode next;
       ListNode(int x) { val = x; }
   }
 
     public static void reorderList(ListNode head) {
+        if (head == null || head.next == null)
+            return;
+
+        ListNode mid = findMiddle(head);
+
+        ListNode tail = reverse(mid.next);
+        mid.next = null;  //设置左半段新的尾巴，相当于把原list截断
+
+        merge(head, tail);
+    }
+
+
+    //双指针 快慢法  找middle
+    private static ListNode findMiddle(ListNode head){
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast != null && fast.next != null){ //这个list不知道是奇偶的长度
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+
+    private static ListNode reverse(ListNode head){
+        ListNode prev = null;  //构建一个指针，指向head的前一个node
+
+        // pre 和 head 想象成两个指针
+        while (head != null){
+            ListNode tmp = head.next;  //先把head的右一个，保存下来
+            head.next = prev;  //把head指向左边的node
+            prev = head;
+            head = tmp;
+        }
+
+        return prev;
+    }
+
+
+    private static void merge(ListNode head1, ListNode head2){
+        ListNode dummyHead = new ListNode(-1);
+
+        int index = 0;
+
+        while (head1 != null && head2 != null){
+            if (index%2 == 0){  //偶数位的index
+                dummyHead.next = head1;
+                head1 = head1.next;
+            }
+            else {  //奇数位的index
+                dummyHead.next = head2;
+                head2 = head2.next;
+            }
+
+            dummyHead = dummyHead.next;  //指针继续往下走
+            index++;
+        }
+
+        if (head1 != null){ //如果head1 还有剩余的，没有merge进去
+            dummyHead.next = head1;
+        }
+        if (head2 != null){
+            dummyHead.next = head2;
+        }
 
     }
+
 
     public static void main(String[] args) {
 

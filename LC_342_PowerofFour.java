fb8e0e1191a53d6c27f456f9d25d1e012e8f49f8
monkeyzxr <monkeyzxr@gmail.com>
2017-08-15 23:38:22 -0500

    Add files via upload

---
+++ LC_342_PowerofFour.java
@@ -0,0 +1,22 @@
+/**
+ * Created by monkeyzxr on 2017/8/15.
+ *
+ Given an integer (signed 32 bits), write a function to check whether it is a power of 4.
+
+ Example:
+ Given num = 16, return true. Given num = 5, return false.
+
+ Follow up: Could you solve it without loops/recursion?
+
+
+ *
+ */
+public class LC_342_PowerofFour {
+    public boolean isPowerOfFour(int num) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
